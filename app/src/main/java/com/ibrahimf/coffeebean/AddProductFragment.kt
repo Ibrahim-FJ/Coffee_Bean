bf512671597114afaf29d8ed198f82c3d090c204
Ibrahim Alfaifi <Ibrahim.farhan.ruh.k@tuwaiq.edu.sa>
2021-12-22 21:10:20 +0300

    create-profile

---
+++ app/src/main/java/com/ibrahimf/coffeebean/AddProductFragment.kt
@@ -0,0 +1,12 @@
+package com.ibrahimf.coffeebean
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class AddProductFragment : Fragment() {
+
+}
---
+++ app/src/main/java/com/ibrahimf/coffeebean/data/RegistrationRemoteDataSource.kt
@@ -0,0 +1,6 @@
+package com.ibrahimf.coffeebean.data
+
+class RegistrationRemoteDataSource {
+
+
+}
